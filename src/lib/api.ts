4844074a476ccf75b401f977f2f683dd060d6b14
triyanadewi <triyanadewifatmawati@gmail.com>
2025-06-19 14:26:21 +0700

    feat: remove with credentials to handle cors issue

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,6 @@ import Cookies from "js-cookie";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api",
-  withCredentials: true,
 });
 
 api.interceptors.request.use(
